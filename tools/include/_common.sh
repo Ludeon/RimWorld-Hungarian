14f0cc0d1d3294cf59d86d7657554ab3eb64068c
gorrt <attila.orban@gmail.com>
2022-08-08 17:38:30 +0200

    Add command line helper to update the game language files

---
+++ tools/include/_common.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+#function include() {
+#    # shellcheck disable=SC1090
+#    . "$DIR/include/${1}.sh"
+#}
+
+function log() {
+  echo "${1}"
+}
+
+function update_core() {
+  log "Update Core..."
+  ls -lat "${CORE_PATH}/Languages/Hungarian (Magyar)/"
+
+}
+
+function clean_royalty() {
+  log "Cleaning Royalty..."
+  TARGET_DIR="${DLC_ROYALTY_PATH}/Languages/${TARGET_LANGUAGE}/Keyed"
+  log "-> ${TARGET_DIR}"
+  rm -r "${TARGET_DIR}"
+}
+
+function update_royalty() {
+  log "Update Royalty..."
+  ls -lat "${DLC_ROYALTY_PATH}"
+  #  ls -lat "${DLC_TEST_PATH}"
+  ls -lat "$DIR/../Royalty"
+  SOURCE_DIR="$DIR/../Royalty"
+  log "source: ${SOURCE_DIR}"
+  cd "${SOURCE_DIR}" || exit
+  TARGET_DIR="${DLC_ROYALTY_PATH}/Languages/${TARGET_LANGUAGE}"
+  log "target: ${TARGET_DIR}"
+  mkdir -p "${TARGET_DIR}"
+  cp -r "./" "${TARGET_DIR}"
+}
+
+function update_ideology() {
+  log "Update Ideology..."
+  ls -lat "${DLC_IDEOLOGY_PATH}"
+}
